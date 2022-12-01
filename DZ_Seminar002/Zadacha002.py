4cf6cc43e6f91c9cd58803c9cea1fd7fca1331cc
Roman <romanols1989@gmail.com>
2022-12-01 12:43:38 +0600

    решение второй задачи второго ДЗ

--- DZ_Seminar002/Zadacha002.py
+++ DZ_Seminar002/Zadacha002.py
@@ -1,7 +1,11 @@
 # Задайте список из n чисел последовательности (1 + 1/n)^n.
 # Вывести в консоль сам список и сумму его элементов.
 n = int(input('Ввелдите число '))
-new_list = [0]*n
-for i in new_list:
-    new_list.append()
+new_list = []
+for i in range(n):
+    new_list.append((1+1/(i+1))**(i+1))
 print(new_list)
+sum = 0
+for i in new_list:
+    sum = sum+i
+print(sum)
